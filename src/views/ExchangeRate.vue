e52e0d623ca4196bf33abd1e87b2a9daf1cc9e3e
Shan Wu <Shan.Wu@ut.ee>
2025-01-01 02:29:46 +0200

    init plugin pages, tailwindcss

--- src/views/ExchangeRate.vue
+++ src/views/ExchangeRate.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+
 </script>
 
 <template>
-  <RouterView />
+<div>This is currency view</div>
 </template>
 
 <style scoped>
---
+++ src/views/PluginNotFound.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen text-center p-4">
+    <svg class="w-16 h-16 text-red-500 mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+    </svg>
+    <h1 class="text-3xl font-bold text-red-500 mb-4">Plugin Not Found</h1>
+    <p class="text-lg text-gray-700 dark:text-gray-100 mb-6">The plugin you are looking for does not exist or has been removed.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
